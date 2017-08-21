5cef40f2e7e5aad7e847c2a26f6b0ab4c2892bd5
Piotr Wawrzyńczyk <Wawrzyn321@gmail.com>
2017-08-21 21:17:36 +0200

    Added mesh saving

---
+++ Procedural sprite generation/Assets/PSG/Scripts/Editor/SaveMeshEditor.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using UnityEditor;
+using PSG;
+
+/// <summary>
+/// 
+/// Saving generated meshes.
+/// 
+/// Meshes and objects will appear in PSG/ directories by default.
+/// To load, simply drag object to scene or load it by script.
+/// 
+/// Watch out for overwritting saved assets!
+/// 
+/// </summary>
+[CustomEditor(typeof(MeshBase), true)]
+public class SaveMeshEditor : Editor
+{
+    //standard override
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        MeshBase targetScript = (MeshBase)target;
+        //save MeshFilter's content
+        if (GUILayout.Button("Save Mesh Only"))
+        {
+            SaveMeshToFile(targetScript._Mesh, targetScript.C_MR.material, targetScript.name);
+        }
+        //save GameObject
+        if (GUILayout.Button("Save Prefab"))
+        {
+            SavePrefabToFile(targetScript, targetScript.name);
+        }
+    }
+
+    //save MeshFilter's mesh
+    private void SaveMeshToFile(Mesh mesh, Material material, string name)
+    {
+        CheckFolders("Saved meshes");
+
+        //save material
+        if (material != null)
+        {
+            SaveMaterial(material, name);
+        }
+
+        AssetDatabase.CreateAsset(mesh, "Assets/PSG/Saved meshes/" + name + ".asset");
+
+        Debug.Log("Mesh \"" + name + ".asset\" saved succesfully at PSG/Saved meshes");
+    }
+
+    //save entire GameObject
+    private void SavePrefabToFile(MeshBase meshBase, string name)
+    {
+        CheckFolders("Saved prefabs");
+
+        //mesh and it's material need to be saved too
+        SaveMeshToFile(meshBase.C_MF.mesh, meshBase.C_MR.material, name + "'mesh");
+
+        PrefabUtility.CreatePrefab("Assets/PSG/Saved prefabs/" + name + ".prefab", meshBase.gameObject);
+
+        Debug.Log("Prefab \"" + name + ".prefab\" saved succesfully at PSG/Saved prefabs");
+    }
+
+    //save material if necessary
+    private void SaveMaterial(Material material, string name)
+    {
+        if(string.IsNullOrEmpty(AssetDatabase.GetAssetPath(material)))
+        {
+            AssetDatabase.CreateAsset(material, "Assets/PSG/Saved meshes/" + name + "'s material.asset");
+        }
+    }
+    
+    //utility: check if folder exists in PSG directory
+    private void CheckFolders(string targetFolder)
+    {
+        //check for PSG
+        if (!AssetDatabase.IsValidFolder("Assets/PSG"))
+        {
+            AssetDatabase.CreateFolder("Assets", "PSG");
+        }
+
+        //check for {targetFolder}
+        if(!AssetDatabase.IsValidFolder("Assets/PSG/"+ targetFolder))
+        {
+            AssetDatabase.CreateFolder("Assets/PSG", targetFolder);
+        }
+    }
+}
