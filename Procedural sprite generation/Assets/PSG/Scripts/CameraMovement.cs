542a5cc47a0d65c19c5e700a53ba5aecfefe2bf6
Piotr Wawrzyńczyk <Wawrzyn321@gmail.com>
2017-08-25 12:58:49 +0200

    Added CameraMovement

---
+++ Procedural sprite generation/Assets/PSG/Scripts/CameraMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+
+    public float zommingSpeed = 0.2f;
+    public float zommingDelta = 2f;
+    public bool allowNegativeSize = false;
+
+    private float currentZoom;
+
+    public float panningSpeed = 0.5f;
+    private Vector3 panPosition;
+    private bool isPanning = false;
+
+
+    void Awake ()
+    {
+        currentZoom = Camera.main.orthographicSize;
+    }
+	
+	void Update ()
+	{
+        currentZoom -= Input.GetAxis("Mouse ScrollWheel")*zommingDelta;
+        if (!allowNegativeSize && currentZoom < 0) currentZoom = 0;
+	    Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize, currentZoom, zommingSpeed);
+
+	    if (Input.GetMouseButtonDown(2))
+	    {
+	        panPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+	        isPanning = true;
+	    }
+	    else if(Input.GetMouseButtonUp(2))
+	    {
+	        isPanning = false;
+	    }
+	    if (isPanning)
+	    {
+	        Vector3 v = panPosition - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+	        transform.position = Vector3.Lerp(transform.position, 
+                transform.position += new Vector3(v.x, v.y, 0),
+	            panningSpeed);
+	    }
+	}
+}
